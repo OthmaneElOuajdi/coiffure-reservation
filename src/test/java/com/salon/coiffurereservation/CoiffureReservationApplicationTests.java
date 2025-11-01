6155cd4ace70b77388ad1e7b91b54f2b7cc17557
Othmane <oelouajdi@hotmail.com>
2025-11-01 20:20:57 +0100

    Initial commit - projet Spring Boot salon de coiffure

---
+++ src/test/java/com/salon/coiffurereservation/CoiffureReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon.coiffurereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoiffureReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
